ae24e2865c067180dda8cfacf5582a7cb1806264
P4r4dis <adil.d.pro@gmail.com>
2023-04-05 18:22:25 +0200

    [ADD] - feature Errors ex00 add

--- ex00/bin/main.cpp
+++ ex00/bin/main.cpp
@@ -1,6 +1,43 @@
 #include "../include/Errors.hpp"
+#include <iostream>
 
 int main(void)
 {
+    try
+    {
+        throw MissionCriticalError("Power failure");
+    }
+    catch (const NasaError& e)
+    {
+        std::cerr << "Error message: " << e.what() << std::endl;
+        std::cerr << "Component name: " << e.getComponent() << std::endl;
+    }
+    try
+    {
+        throw LifeCriticalError("LifeCriticalError failure");
+    }
+    catch (const NasaError& e)
+    {
+        std::cerr << "Error message: " << e.what() << std::endl;
+        std::cerr << "Component name: " << e.getComponent() << std::endl;
+    }
+    try
+    {
+        throw UserError("UserError failure", "UserError component");
+    }
+    catch (const NasaError& e)
+    {
+        std::cerr << "Error message: " << e.what() << std::endl;
+        std::cerr << "Component name: " << e.getComponent() << std::endl;
+    }
+        try
+    {
+        throw CommunicationError("CommunicationError failure");
+    }
+    catch (const NasaError& e)
+    {
+        std::cerr << "Error message: " << e.what() << std::endl;
+        std::cerr << "Component name: " << e.getComponent() << std::endl;
+    }
     return 0;
 }
